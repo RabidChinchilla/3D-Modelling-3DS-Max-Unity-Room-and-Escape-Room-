8a031929d941783eec5448223d63861625bb5e1f
RabidChinchilla <33025242+RabidChinchilla@users.noreply.github.com>
2018-10-03 11:40:00 +0100

    put door and animtion into unity

---
+++ Door Scene/Assets/Scripts/PlayAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour {
+
+    public bool hasBeenOpened;
+
+    private Animator doorOpening;
+	// Use this for initialization
+	void Start () {
+        hasBeenOpened = true;
+
+        doorOpening = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
