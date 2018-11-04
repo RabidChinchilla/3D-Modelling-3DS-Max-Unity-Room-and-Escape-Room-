9297c24b2cb347ecf446be2fb4d911ca5464fc30
RabidChinchilla <bengearing5@gmail.com>
2018-11-04 22:43:00 +0000

    Working on the door scene
    Added some materials to objects, made the door open when you press a button and added some sounds to make the scene more atmospheric

---
+++ Door Scene/Assets/Scripts/PlaySoundOnCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnCollision : MonoBehaviour {
+
+    public AudioClip alien;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = alien;
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter () {
+        GetComponent<AudioSource>().Play();
+	}
+}
