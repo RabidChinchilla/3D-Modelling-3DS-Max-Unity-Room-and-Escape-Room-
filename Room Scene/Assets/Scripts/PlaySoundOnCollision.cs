86151c3868f995ba4ae7cc9ec045aaa03f2ae3ff
RabidChinchilla <33025242+RabidChinchilla@users.noreply.github.com>
2018-11-09 01:00:02 +0000

    last minute scene editing
    forgot to add some sounds to the scene so did so asap

---
+++ Room Scene/Assets/Scripts/PlaySoundOnCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnCollision : MonoBehaviour {
+
+    public AudioClip alien;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = alien;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerStay () {
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+	}
+}
