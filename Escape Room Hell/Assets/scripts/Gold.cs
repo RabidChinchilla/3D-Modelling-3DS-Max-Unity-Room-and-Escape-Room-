4081896604201a3ab40943dd83d3dd665915bef4
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-01-16 18:45:36 +0000

    started scripting for puzzles
    started scripting for the puzzles so that things can be picked up

---
+++ Escape Room Hell/Assets/scripts/Gold.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gold : MonoBehaviour {
+
+    public GameObject Player;
+
+    private void OnTriggerStay(Collider other)
+    {
+        LevelProgression levelProgression = Player.GetComponent<LevelProgression>();
+        if (Input.GetKeyDown("e"))
+        {
+            levelProgression.goldCollected = true;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Escape Room Hell/Assets/scripts/LevelProgression.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelProgression : MonoBehaviour {
+
+    public bool swordCollected = false;
+    public bool cakeCollected = false;
+    public bool goldCollected = false;
+    public bool soulCorrupted = false;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
