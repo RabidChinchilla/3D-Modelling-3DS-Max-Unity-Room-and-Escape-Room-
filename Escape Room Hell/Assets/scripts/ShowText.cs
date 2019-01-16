b4036863873b850f6f426ecac36bfa5f800c51a4
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-01-16 17:02:05 +0000

    added animation and UI
    added animation other corrupt souls, added a ui and a script to show text when people click a button to inspect

---
+++ Escape Room Hell/Assets/scripts/ShowText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowText : MonoBehaviour {
+
+    public GameObject clue;
+    public GameObject inspect;
+
+    private void OnTriggerStay(Collider other)
+    {
+        inspect.gameObject.SetActive(true);
+
+        if (Input.GetKeyDown("e"))
+        {
+            clue.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inspect.gameObject.SetActive(false);
+        clue.gameObject.SetActive(false);
+    }
+}
